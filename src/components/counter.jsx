c3fa796c6d9b3148e8f3c9c690e29554b9561f6a
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-14 14:42:33 -0700

    learned about the map method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,13 +9,24 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tags.map(tag=><li key={tag}>{tag}</li>)} 
+            {/* render dynamically first so this.state */}
+            {/* .tags is the name of the array */}
+            {/* now use the map method and pass tag (tag=><li></li>) and map it to the array items */}
+            {/* in jsx, you cant loop through an array so we need to use map method */}
+            {/* to map EACH element on the array to render */}
+            {/* whenever you use map method to map through array, you need to set key attribute key = {} */}
+            {/* key is unique only on that list */}
+        </ul>
       </div>
     );
   }
