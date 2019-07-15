abbb0981b2770ecccd8b02bfbd989b699a6cc4d2
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-15 13:41:13 -0700

    Created a new component Counters. New lecture

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,54 +9,89 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    count: 0
   };
-// Conditional rendering 
-  renderTags () {
-      if (this.state.tags.length === 0) return <p>There are no tags!</p>; // this is the case for empty array
-      return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>; // this is the case if there are elements in the array
-    //   {this.renderTags()}  in return method inside render function this is how you call the renderTags() function
+
+  handleIncrement = (product) => {
+    console.log(product)
+    // This is an eventhandler function
+    // always name functions/methods on what they're suppose to do 
+    // to not confuse other developers and yourself
+    this.setState({count: this.state.count+1}); // you need to pass an object and property 
+    // this.setState is a react method
   };
 
   render() {
     return (
       <div>
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span> */}
-        {/* <button className="btn btn-secondary btn-sm">Increment</button> */}
-        <ul>
-            {/* {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}  */}
-            {this.state.tags.length === 0 && 'please create a new tag'} 
-            {/* to conditionally render content, you can use the (&&) operator  */}
-            {this.renderTags()}
-            {/* render dynamically first so this.state */}
-            {/* .tags is the name of the array */}
-            {/* now use the map method and pass tag (tag=><li></li>) and map it to the array items */}
-            {/* in jsx, you cant loop through an array so we need to use map method */}
-            {/* to map EACH element on the array to render */}
-            {/* whenever you use map method to map through array, you need to set key attribute key = {} */}
-            {/* key is unique only on that list */} 
-        </ul>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={ () => this.handleIncrement() } // OnClick is one of the many DOM event properties in JS 
+          // when you wanna pass something on the event handler, 
+          // just add arrow function and the object you wanna pass on the eventhandler
+          className="btn btn-secondary btn-sm ml-2 mt-2"
+          >
+          Increment
+        </button>
       </div>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge-m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-
- 
-//   getBadgeClasses() {
-//     let classes = "badge-m-2 badge-";
-//     classes += this.state.count === 0 ? "warning" : "primary";
-//     return classes;
-//   }
-
-//   formatCount() {
-//     const { count } = this.state;
-//     return count === 0 ? "ZERO" : count;
-//   }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "ZERO" : count;
+  }
 }
 
-
-
-
 export default Counter;
+
+// CONDITIONAL RENDERING  
+/**
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>; // this is the case for empty array
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+         {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}  
+          {this.state.tags.length === 0 && 'please create a new tag'}  
+          to conditionally render content, you can use the (&&) operator  
+          first is the condition and then && and then whatever you want to render could be plain text or jsx expression 
+          {this.renderTags()} 
+          render dynamically first so this.state 
+          .tags is the name of the array 
+          now use the map method and pass tag (tag=><li></li>) and map it to the array items 
+          in jsx, you cant loop through an array so we need to use map method 
+          to map EACH element on the array to render 
+          whenever you use map method to map through array, you need to set key attribute key = {} 
+          key is unique only on that list 
+      </ul>
+    ); // this is the case if there are elements in the array
+    //   {this.renderTags()}  in return method inside render function this is how you call the renderTags() function
+  }
+
+*/
+
+// BINDING EVENT HANDLERS
+// 'this' behaves differently in javascript compare to other languages
+// it can refer to different objects
+
+// constructor() {
+//   super();
+//   this.handleIncrement = this.handleIncrement.bind(this); // this will always refer to the method that was defined i.e handleIncrement
+// because of the bind method.
+// this will return a new instance of the handleIncrement
+// this will always reference the Counter object
+// you need to build a constructor to bind 'this' and user super() to call the constructor
+// of the parent class i.e Counter
+// }
+
+// The other way to bind 'this' is to convert the eventhandler into an arrow function
+// handleIncrement = () => {}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter'; // importing the counter class from the jsx file 
+import Counters from './components/counters'; // importing the counter class from the jsx file 
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Counters />, document.getElementById('root'));
 serviceWorker.unregister();
